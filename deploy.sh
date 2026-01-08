8757f11253c7293106cc0a3aa9e608b9990fd898
conho-web <conho@internet.ru>
2026-01-08 14:05:52 +0300

    test new deploy

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env sh
 
-# abort on errors
 set -e
 
-# build
 npm run build
 
-# navigate into the build output directory
 cd dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:conho-web/conho-web.github.io.git main
+git push -f git@github.com:conho-web/conho-web.github.io.git main:gh-pages
 
 cd -
